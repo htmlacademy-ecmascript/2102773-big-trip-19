062a3911daf636c4dde685b9e636aa1d164c86d0
gerbertwest <gerbert.west@gmail.com>
2022-12-19 14:18:27 +0300

    3.11. Революция или эволюция? (часть 2)

--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@ const bodyElement = document.querySelector('.page-body');
 const filtersElement = bodyElement.querySelector('.trip-controls__filters');
 const sortElement = bodyElement.querySelector('.trip-events');
 const tripInfoElement = bodyElement.querySelector('.trip-main');
+
 const pointsModel = new PointsModel();
+
 const boardPresenter = new BoardPresenter({
   infoContainer: tripInfoElement,
   filterContainer: filtersElement,
